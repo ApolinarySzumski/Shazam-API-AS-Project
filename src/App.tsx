1d6c93129587f79d77e45bc9748249c85dd5a168
ApolinarySzumski <apolinary@poczta.onet.pl>
2024-04-02 12:31:18 +0200

    basics of app

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,90 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// Aplikacja używa następującego API: https://openweathermap.org/current
+
+import { useEffect, useState } from "react";
+import "./App.css";
+
+const API_KEY = "00cf304a06131d3dfb70f11fd32c7eba";
+const latSuwalki = 54.11;
+const lonSuwalki = 22.93;
+
+type Weather = {
+  name: string;
+  main: {
+    temp: number;
+    pressure: number;
+  };
+};
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [weatherConditions, setWeatherConditions] = useState<Weather>({
+    name: "",
+    main: {
+      temp: 0,
+      pressure: 0,
+    },
+  });
+  const [isButtonClicked, setIsButtonClicked] = useState(false);
+
+  const getWeather = async () => {
+    try {
+      // Api params : lat - szerokość geograficzna, lon - długość geograficzna, units - okreśkla jednostkę temperatury, API_KEY - klucz dostępu
+      const resp = await fetch(
+        `https://api.openweathermap.org/data/2.5/weather?lat=${latSuwalki}&lon=${lonSuwalki}&units=metric&appid=${API_KEY}`,
+      );
+      console.log(resp);
+      const respToExtract = await resp.json();
+      return respToExtract;
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getWeather()
+      .then((extractedResp) => {
+        setWeatherConditions(extractedResp);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
+  const handleClick = () => {
+    setIsButtonClicked(false);
+    console.log(weatherConditions);
+    setIsButtonClicked(true);
+  };
+
+  const renderWeather = () => {
+    return (
+      <div>
+        <p>{weatherConditions.name}</p>
+        <br />
+        <p>Aktualna Temperatura: {weatherConditions.main.temp}</p>
+        <br />
+        <p>Ciśnienie: {weatherConditions.main.pressure}hPa</p>
+        <br />
+      </div>
+    );
+  };
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      {/* <form onSubmit={handleSubmit}>
+        <label>
+          Type song name
+          <input
+            type="text"
+            placeholder="Enter song name"
+            value={inputValue}
+            onChange={handleChange}
+          />
+        </label> */}
+      <button onClick={handleClick}>Sprawdź aktualną pogodę</button>
+      {isButtonClicked === false ? <p>naciśnij przycisk</p> : renderWeather()}
+      {/* </form> */}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
